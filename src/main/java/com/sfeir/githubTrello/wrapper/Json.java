f6bc6eb488f0d923f219a9eafc51c1a1df339a62
MEDDAH Julien <julien@meddah.fr>
2012-12-03 10:42:37 +0100

    Dead code removed

--- src/main/java/com/sfeir/githubTrello/wrapper/Json.java
+++ src/main/java/com/sfeir/githubTrello/wrapper/Json.java
@@ -2,15 +2,11 @@ package com.sfeir.githubTrello.wrapper;
 
 import java.io.IOException;
 import java.util.Collection;
-import java.util.Iterator;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.ObjectMapper;
-import org.codehaus.jackson.node.ArrayNode;
 
 import com.sfeir.githubTrello.domain.github.Branch;
 import com.sfeir.githubTrello.domain.github.Branch.Commit;
@@ -65,37 +61,22 @@ public final class Json {
 	}
 
 	public static String extractValue(String json, String... attributes) {
-		JsonNode node = fromJsonToObject(json, JsonNode.class);
-		for (String attribute : attributes) {
-			node = node.isArray() ? fromMultipleNodes(attribute, ((ArrayNode) node)) : node.get(attribute);
+		try {
+			JsonNode node = mapper.readTree(json);
+			for (String attribute : attributes)
+				node = node.get(attribute);
+			return node.getTextValue();
+		} catch (IOException e) {
+			logger.error(e, e);
+			return "";
 		}
-		return node.getTextValue();
-	}
-
-
-	private static JsonNode fromMultipleNodes(String attribute, ArrayNode node) {
-		Matcher matcher = Pattern.compile("(.*)\\[(.*)=(.*)\\]").matcher(attribute);
-		if (!matcher.matches()) {
-			logger.warn("Json node with multiple elements with attribute: " + attribute);
-		}
-		String actualAttribute = matcher.group(1);
-		String key = matcher.group(2);
-		String value = matcher.group(3);
-
-		Iterator<JsonNode> elements = node.getElements();
-		while (elements.hasNext()) {
-			JsonNode element = elements.next();
-			if (value.equals(element.get(key).getTextValue())) {
-				return element.get(actualAttribute);
-			}
-		}
-		return node;
 	}
 
 	private Json() {}
 
 	private static final ObjectMapper mapper = new ObjectMapper();
 	private static final Log logger = LogFactory.getLog(Json.class);
+
 	static {
 		mix(Branch.class, BranchMixin.class);
 		mix(Commit.class, CommitMixin.class);
